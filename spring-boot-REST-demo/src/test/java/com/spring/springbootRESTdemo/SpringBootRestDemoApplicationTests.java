9951f033427d9f9f5fb474f06a875f198157a8e2
JonOlsso <JonOlsso@DESKTOP-42OD9MV.Ductus.local>
2019-11-14 12:48:59 +0100

    Created spring boot REST demo.

---
+++ spring-boot-REST-demo/src/test/java/com/spring/springbootRESTdemo/SpringBootRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootRESTdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
